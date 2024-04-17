bb393b97a3bc3ee27b848f78349632c8e2edfd88
김가현 <gahyun3722@gmail.com>
2024-04-17 14:34:09 +0900

    중간 1차 수정

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,3 +1,19 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
 export function intro(){
-    
+    gsap.registerPlugin(ScrollTrigger)
+    const frameCount=15
+    let offsetValue=150
+    gsap.to('.img',{
+        backgroundPosition:(-offsetValue*frameCount)+'px',
+        ease:'steps('+frameCount+')',
+        scrollTrigger:{
+            trigger:'#intro',
+            start:'top top',
+            end:'+='+(frameCount*offsetValue),
+            pin:true,
+            scrub:true,
+        }
+    })
 }
